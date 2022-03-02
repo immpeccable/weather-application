a1a51891038ece008e90a7bb618853ad1381402f
tunahandündar <tunahan.dundar.2221@gmail.com>
2022-03-02 22:02:47 +0300

    functionality is completed

---
+++ src/index.js
@@ -0,0 +1,67 @@
+let apiBegin = "http://api.openweathermap.org/data/2.5/weather?q=";
+let apiEnd = "&APPID=baead7b630649327b2d7239548d6e961";
+let searchCity = document.getElementById("search-city");
+let searchButton = document.getElementById("search-button");
+let errorMessage = document.getElementById("error-msg");
+let weatherIdentifier = document.getElementById("weather-identifier");
+let cityName = document.getElementById("city-name");
+let degreeValue = document.getElementById("degree");
+let feelsLikeValue = document.getElementById("feels-like");
+let windValue = document.getElementById("wind");
+let humidityValue = document.getElementById("humidity");
+findCity("Ankara");
+
+
+searchButton.addEventListener('click', () => {
+
+    let city = searchCity.value;
+    findCity(city);
+})
+
+function twoDecimal(number){
+    return Math.round((number + Number.EPSILON) * 100) / 100;
+    
+}
+
+async function findCity(city) {
+
+    try {
+        let object = await fetch(apiBegin + city + apiEnd, { mode: 'cors' })
+        let response = await object.json();
+        console.log(response);
+        
+        let humidity = twoDecimal(response.main.humidity);
+        let celcius = Math.round(response.main.temp - 272);
+        let feelsLike = twoDecimal(response.main.feels_like - 272);
+        
+        let dscr = response.weather[0].description;
+        let wnd = response.wind.speed
+
+        feelsLikeValue.textContent = `FEELS LIKE: ${feelsLike}°C`
+        windValue.textContent = `WIND: ${wnd} km/h`;
+        humidityValue.textContent = `HUMIDITY: ${humidity}%`;
+        degreeValue.innerHTML = `${celcius} <div class = "celcius-image">
+        °C
+    </div>`;
+        cityName.textContent = city.toUpperCase();
+        weatherIdentifier.textContent = dscr.toUpperCase();
+
+        errorMessage.textContent = "";
+        
+    }
+    catch (error) {
+
+        
+        errorMessage.textContent = `No location found for ${city}`
+    }
+}
+/*fetch(apiUrl, { mode: 'cors' })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function(err){
+        console.log(err);
+    })*/
